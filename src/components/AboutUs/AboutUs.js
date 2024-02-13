fa80774c75af7d569dde4a6b2cf3e1c770d38c23
danyal <danyal.titanka24@gmail.com>
2024-02-13 17:54:03 +0330

    AboutBox Updated

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -6,7 +6,7 @@ import { MoreInfo } from './MoreInfo';
 export const AboutUs = () => {
 
   const [aboutUs, setAboutUs] = useState([
-    { id: 1, firstName: "دانیال", lastName: "لطفی", age: 19, area: "Frontend", aboutMe: "Lorem ipsum dolor sit amet.", img: "../images/danyal.jpg" },
+    { id: 1, firstName: "دانیال", lastName: "لطفی", age: 19, area: "Frontend", aboutMe: "Lorem ipsum dolor sit amet.", img: "../images/danyal.png" },
     { id: 2, firstName: "امین", lastName: "قاسمی", age: 20, area: "Backend", aboutMe: "Lorem ipsum dolor sit amet.", img: "../images/1101238.jpg" },
     { id: 3, firstName: "سارا", lastName: "مرادخانی", age: 21, area: "Database", aboutMe: "Lorem ipsum dolor sit amet.", img: "../images/1101238.jpg" },
     { id: 4, firstName: "اشکان", lastName: "طاهرنیا", age: 19, area: "Wordpress", aboutMe: "Lorem ipsum dolor sit amet.", img: "../images/1101238.jpg" },
